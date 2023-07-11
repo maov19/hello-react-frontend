e382debdb60ae6fa433c4ff14b4b20811d13a511
mossa <martin.ossa.8@gmail.com>
2023-07-11 14:53:08 +0200

    Setup react-app and components

---
+++ src/components/Greeting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Greeting = () => {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <p>This is a greeting message.</p>
+    </div>
+  );
+};
+
+export default Greeting;
